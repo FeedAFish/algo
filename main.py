fcd7db8a661cd3dffb6bd358de001d054cabd8d3
DineJ <dinejridi@gmail.com>
2026-04-02 10:06:18 +0200

    Updated script to hide api's key. It loads the key from environnement

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import json  # Format JSON
 import time  # Gère le temps
 import os  # Interaction avec le système
 from API.script.api import Api 
+from dotenv import load_dotenv
 
-api_key = "a28db3a7-7c19-4721-b1c7-bb5c76250566"  # Variable globale qui sera la clef d'authentification de l'API
+load_dotenv()  # charge le .env
+
+api_key = os.getenv("API_KEY") # Charge la clef API depuis l'env
 url_api = "https://api.datatourisme.fr/v1/catalog"  # L'url de base de l'API
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # Chemin 
-path_output = os.path.join(BASE_DIR, "API", "data_raw_ndjson") # Dossier de sauvegarde des données brutes au format NDJSON
-path_state = os.path.join(BASE_DIR, "API", "checkpoint.json")  # Fichier qui garde la progression de l'extraction pour pouvoir reprendre en cas d'arrêt
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # Chemin
+path_output = os.path.join(BASE_DIR, "API", "data_raw2_ndjson") # Dossier de sauvegarde des données brutes au format NDJSON
+path_state = os.path.join(BASE_DIR, "API", "checkpoint2.json")  # Fichier qui garde la progression de l'extraction pour pouvoir reprendre en cas d'arrêt
 
 
 page_file = 1000  # Nombre de pages à stocker dans un même fichier
