dd6f6bbcadb492f24d08d0d02297011b792d0f93
DineJ <dinejridi@gmail.com>
2026-04-08 09:01:44 +0200

    Fix path

--- data_download/__main__.py
+++ data_download/__main__.py
@@ -3,8 +3,8 @@ import requests  # Requête HTTP
 import json  # Format JSON
 import time  # Gère le temps
 import os  # Interaction avec le système
-from .script.datatourisme_download import DatatoursimeDownload 
-from .script.picture import Picture
+from .downloader.datatourisme_download import DatatoursimeDownload
+from .downloader.picture import Picture
 from dotenv import load_dotenv
 
 load_dotenv()  # charge le .env
@@ -14,9 +14,9 @@ api_key = os.getenv("API_KEY") # Charge la clef API depuis l'env
 url_api = "https://api.datatourisme.fr/v1/catalog"  # L'url de base de l'API
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # Chemin
-path_output = os.path.join(BASE_DIR, "../","data_lake") # Dossier de sauvegarde des données brutes au format NDJSON
-path_state = os.path.join(BASE_DIR, "checkpoint.json")  # Fichier qui garde la progression de l'extraction pour pouvoir reprendre en cas d'arrêt
-path_picture = os.path.join(BASE_DIR, "pictures")
+path_output = os.path.join(BASE_DIR, "..","data_lake") # Dossier de sauvegarde des données brutes au format NDJSON
+path_state = os.path.join(BASE_DIR, "../data_lake", "checkpoint.json")  # Fichier qui garde la progression de l'extraction pour pouvoir reprendre en cas d'arrêt
+path_picture = os.path.join(BASE_DIR, "..", "pictures")
 
 page_file = 1000  # Nombre de pages à stocker dans un même fichier
 page_size = 21  # Nombre d'objets demandés par page à l'API
